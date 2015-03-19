8768cb1ccb0053e91f0ac8cff158f20cbaf239a3
exceptionnotfound <mpjones428@gmail.com>
2015-03-19 11:50:49 -0700

    First checkin of ModelStateDemo

---
+++ ModelStateDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ModelStateDemo.ViewModels.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelStateDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            AddUserVM model = new AddUserVM();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(AddUserVM model)
+        {
+            if(model.FirstName == model.LastName)
+            {
+                ModelState.AddModelError("LastName", "The last name cannot be the same as the first name.");
+            }
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ModelStateDemo/ViewModels/Home/AddUserVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelStateDemo.ViewModels.Home
+{
+public class AddUserVM
+{
+    [Required(ErrorMessage = "Please enter the user's first name.")]
+    [StringLength(50, ErrorMessage = "The First Name must be less than {1} characters.")]
+    [Display(Name = "First Name:")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Please enter the user's last name.")]
+    [StringLength(50, ErrorMessage = "The Last Name must be less than {1} characters.")]
+    [Display(Name = "Last Name:")]
+    public string LastName { get; set; }
+
+    [EmailAddress(ErrorMessage = "The Email Address is not valid")]
+    [Required(ErrorMessage = "Please enter an email address.")]
+    [Display(Name = "Email Address:")]
+    public string EmailAddress { get; set; }
+}
+}
